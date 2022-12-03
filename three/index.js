b5c46f411e0206641462e43686600709545b33e9
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-02 23:26:17 -0600

    part 1 done

--- three/index.js
+++ three/index.js
@@ -2,4 +2,44 @@ import { readFile } from './reader.js';
 
 const data = await readFile();
 
-// const rounds = data.split('\n');
+const sacks = data.split('\n');
+
+let totalScore = 0;
+
+// let elfCount = 0;
+
+for (const sack of sacks) {
+  const one = sack.substring(0, sack.length / 2);
+  const two = sack.substring(sack.length / 2);
+
+  // const oneI = 0;
+  let commonChar = undefined;
+  for (const oneChar of [...one]) {
+    
+    for (const twoChar of [...two]) {
+      if (oneChar === twoChar) {
+        // found!
+        commonChar = oneChar;
+        break;
+      }
+    }
+    if (commonChar) {
+      break;
+    }
+  }
+
+  if (!commonChar) {
+    throw new Error('not found!')
+  }
+
+  const subtractor = (commonChar === commonChar.toUpperCase()) ? (65 - 27) : 96;
+  let score = commonChar.charCodeAt(0) - subtractor;
+  totalScore += score;
+  console.log(`${commonChar}: ${score}. ${totalScore}`)
+}
+
+
+// a - z = 1 - 26
+// A - Z = 27 - 52
+// 65 for upper. 97 for lower.
+console.log(totalScore)
