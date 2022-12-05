c203c1873fb70d71049e39d8e0f60ea4019ca4c9
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-04 23:41:50 -0600

    finished day 5

--- five/index.js
+++ five/index.js
@@ -1,41 +1,119 @@
 import { readFile } from './reader.js';
 
-const data = await readFile();
-const elves = data.split('\n');
+// const data = await readFile();
+const instructions = await readFile('./instructions.txt');
+   
+// This is lame-o...
+const one = [];
+one.push('Z');
+one.push('T');
+one.push('F');
+one.push('R');
+one.push('W');
+one.push('J');
+one.push('G');
 
-// let total = 0;
+const two = [];
+two.push('G');
+two.push('W');
+two.push('M');
 
-// let i = 0;
-// for (const pair of elves) {
-//   const thePairs = pair.split(',');
-//   const one = thePairs[0];
-//   const two = thePairs[1];
+const three = [];
+three.push('J');
+three.push('N');
+three.push('H');
+three.push('G');
 
-//   const oneEnds = one.split('-');
-//   const twoEnds = two.split('-');
+const four = [];
+four.push('J');
+four.push('R');
+four.push('C');
+four.push('N');
+four.push('W');
 
-//   const oneStart = +oneEnds[0];
-//   const oneEnd = +oneEnds[1];
+const five = [];
+five.push('W');
+five.push('F');
+five.push('S');
+five.push('B');
+five.push('G');
+five.push('Q');
+five.push('V');
+five.push('M');
 
-//   const twoStart = +twoEnds[0];
-//   const twoEnd = +twoEnds[1];
+const six = [];
+six.push('S');
+six.push('R');
+six.push('T');
+six.push('D');
+six.push('V');
+six.push('W');
+six.push('C');
 
-//   if (oneStart >= twoStart && oneEnd <= twoEnd
-//     || oneStart <= twoStart && oneEnd >= twoEnd) {
-//     total++;
-//     continue;
-//     // console.log(`${i} - ${pair}. Total: ${total} `);
-//   }
+const seven = [];
+seven.push('H');
+seven.push('B');
+seven.push('N');
+seven.push('C');
+seven.push('D');
+seven.push('Z');
+seven.push('G');
+seven.push('V');
 
-//   if (oneStart >= twoStart && oneStart <= twoEnd
-//     || twoStart >= oneStart && twoStart <= oneEnd
-//     || oneEnd <= twoEnd && oneEnd >= twoStart
-//     || twoEnd <= oneEnd && twoEnd >= oneStart) {
-//     total++;
-//   }
-//   i++;
-// }
+const eight = [];
+eight.push('S');
+eight.push('J');
+eight.push('N');
+eight.push('M');
+eight.push('G');
+eight.push('C');
 
-// const sacks = data.split('\n');
+const nine = [];
+nine.push('G');
+nine.push('P');
+nine.push('N');
+nine.push('W');
+nine.push('C');
+nine.push('J');
+nine.push('D');
+nine.push('L');
 
-console.log(total);
+const fatStacks = [];
+fatStacks.push(one);
+fatStacks.push(two);
+fatStacks.push(three);
+fatStacks.push(four);
+fatStacks.push(five);
+fatStacks.push(six);
+fatStacks.push(seven);
+fatStacks.push(eight);
+fatStacks.push(nine);
+
+const ins = instructions.split('\n');
+for (const move of ins) {
+  const numMoves = +move.substring(5, 7);
+  const from = +move.substring(12, 14);
+  const to = +move.substring(move.length - 1);
+
+  if (!from) {
+    console.log('blah')
+  }
+
+  const newStack = [];
+
+  for (let i = 0; i < numMoves; i++) {
+    // move this many times
+    const stack = fatStacks[from - 1];
+    const oldOne = stack.pop();
+    newStack.push(oldOne);
+    // For part 1
+    // fatStacks[to - 1].push(oldOne);
+  }
+
+  while (newStack.length > 0) {
+    const oldOne = newStack.pop();
+    fatStacks[to - 1].push(oldOne);
+  }
+}
+
+console.log(fatStacks);
