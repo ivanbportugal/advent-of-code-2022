37cd0be450c960d66ec0423b73713cc60dfee6c2
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-05 23:22:15 -0600

    completed day 6

--- six/index.js
+++ six/index.js
@@ -1,6 +1,25 @@
 import { readFile } from './reader.js';
 
-const data = await readFile();
-const elves = data.split('\n');
+const stream = await readFile();
+const charArray = stream.split('');
 
-console.log(fatStacks);
+const markers = [];
+
+for (let i = 0; i < charArray.length - 14; i++) {
+
+  const values = [];
+  for (let j = 0; j < 14; j++) {
+    values.push(charArray[i + j]);
+  }
+  
+  if (values.length === new Set(values).size) {
+    // unique
+    console.log(`unique: ${i}: ${values}`)
+    markers.push(i + 3);
+  } else {
+    // not unique
+    console.log(`not unique: ${i}: ${values}`)
+  }
+}
+
+console.log(markers);
