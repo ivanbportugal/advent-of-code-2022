8025e96306f81e8aeb8272a30a1480d80af58203
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-06 22:41:31 -0600

    ready for day 7

---
+++ seven/index.js
@@ -0,0 +1,25 @@
+import { readFile } from './reader.js';
+
+const stream = await readFile();
+const data = stream.split('\n');
+
+// const markers = [];
+
+// for (let i = 0; i < charArray.length - 14; i++) {
+
+//   const values = [];
+//   for (let j = 0; j < 14; j++) {
+//     values.push(charArray[i + j]);
+//   }
+  
+//   if (values.length === new Set(values).size) {
+//     // unique
+//     console.log(`unique: ${i}: ${values}`)
+//     markers.push(i + 3);
+//   } else {
+//     // not unique
+//     console.log(`not unique: ${i}: ${values}`)
+//   }
+// }
+
+console.log(markers);
