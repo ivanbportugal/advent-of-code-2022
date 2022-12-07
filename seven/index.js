2f5b68e5bea9d157f4ec920fca54d94ba053a437
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-07 00:22:59 -0600

    stuck

--- seven/index.js
+++ seven/index.js
@@ -3,23 +3,98 @@ import { readFile } from './reader.js';
 const stream = await readFile();
 const data = stream.split('\n');
 
-// const markers = [];
+const root = {
+  parent: undefined,
+  name: 'root',
+  files: [],
+  totalFiles: 0,
+  childDirs: []
+};
 
-// for (let i = 0; i < charArray.length - 14; i++) {
+let currentDir = root;
 
-//   const values = [];
-//   for (let j = 0; j < 14; j++) {
-//     values.push(charArray[i + j]);
-//   }
+let isListing = false;
+for (const command of data) {
+  if (command === '$ cd /') {
+    // Don't care about the root (there's only one)
+    console.log('skipped root');
+    continue;
+  }
+
+  if (command === '$ ls') {
+    isListing = true;
+    continue;
+  }
   
-//   if (values.length === new Set(values).size) {
-//     // unique
-//     console.log(`unique: ${i}: ${values}`)
-//     markers.push(i + 3);
-//   } else {
-//     // not unique
-//     console.log(`not unique: ${i}: ${values}`)
-//   }
-// }
+  if (command.indexOf('$ cd') > -1) {
+    isListing = false;
+    if(command === '$ cd ..') {
+      // go up a directory
+      currentDir = currentDir.parent;
+    } else {
+      // go down a directory
+      const dirName = command.substring(5);
+      currentDir = currentDir.childDirs.find((item) => item.name === dirName);
+    }
+    continue;
+  }
 
-console.log(markers);
+  if (command.indexOf('dir ') > -1) {
+    // dir
+    let newDir = {
+      parent: currentDir,
+      name: command.substring(4),
+      files: [],
+      totalFiles: 0,
+      childDirs: []
+    }
+    if (currentDir.childDirs.find((child) => child.name === command.substring(4)) !== undefined) {
+      console.log('repeat!')
+    } else {
+      currentDir.childDirs.push(newDir);
+    }
+  } else {
+    // file
+    const theFile = command.split(' ');
+    const size = theFile[0];
+    const fileName = theFile[1];
+    currentDir.files.push({ fileName, size: +size });
+
+    currentDir.totalFiles += (+size);
+  }
+
+  // Not listing a directory, or changing directories
+}
+
+let entireSizeUnder100k = 0;
+// TODO Traverse tree and add them up (recursive)
+const goDeep = (node) => {
+  
+  let totalSum = node.totalFiles;
+  if (node.childDirs.length > 0) {
+    for (let i = 0; i < node.childDirs.length; i++) {
+      const childDir = node.childDirs[i];
+      const childDirSum = goDeep(childDir);
+      totalSum += childDirSum;
+    }
+    if (totalSum < 300000) {
+      // report!
+      console.log('******found! ' + node.name + ' - ' + totalSum)
+      entireSizeUnder100k += totalSum;
+    } else {
+      console.log('total! ' + node.name + ' - ' + totalSum)
+    }
+  }
+
+
+  return totalSum;
+  // const filesSum = node.files.reduce((acc, val) => {
+  //   return val.size + acc
+  // }, 0);
+  // const dirSum
+  // node.totalFiles = filesSum
+}
+
+goDeep(root);
+
+console.log(entireSizeUnder100k);
