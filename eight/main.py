6ee473a8b55a0d4cdf631dd96e1f7619edca6b4e
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-08 20:22:18 -0600

    in progress pythonish

--- eight/main.py
+++ eight/main.py
@@ -1,5 +1,5 @@
 
-import re
+# import re
 
 trees = []
 visible_count = 0
@@ -7,66 +7,39 @@ visible_count = 0
 with open("./input.txt") as fp:
   for line in fp:
     characters = list(line)
-    numbers = [int(x) for x in characters]
+    numbers = [int(x) for x in characters if x != '\n']
     trees.append(numbers)
 
-def checkrow(i):
-  tree_row = trees[i]
-  prev_tree = tree_row[0]
-  next_tree = len(tree_row) - 1
+def right_visible(i, j):
+  for k in range(j, len(trees[i]) - 1):
+    if trees[i][k] < trees[i][k + 1]:
+      return False
+  return True
 
-  done_with_left_right = False
-  for j in range(len(tree_row) / 2):
-    if j == 0:
-      # Don't count first and last tree
+def left_visible(i, j):
+  for k in range(j, 0, -1):
+    if trees[i][k] < trees[i][k - 1]:
+      return False
+  return True
+
+def bottom_visible(i, j):
+  for l in range(i, len(trees) - 1):
+    if trees[l][j] < trees[l + 1][j]:
+      return False
+  return True
+
+def top_visible(i, j):
+  for l in range(i, 0, -1):
+    if trees[l][j] < trees[l - 1][j]:
+      return False
+  return True
+
+visible_count += (len(trees) * 2)
+visible_count += (len(trees[0]) * 2 - 2)
+
+for i in range(1, len(trees) - 1):
+  for j in range(1, len(trees[i]) - 1):
+    if top_visible(i, j) or left_visible(i, j) or bottom_visible(i, j) or right_visible(i, j):
       visible_count += 1
-      continue
 
-    if not done_with_left_right and prev_tree <= tree_row[j]:
-      visible_count += 1
-      prev_tree = tree_row[j]
-    else:
-      # All others to the halway are not visible left to right
-      # Check top row
-      done_with_left_right = True
-      def recurse_up(curr_row):
-        if curr_row == 0:
-          return True
-        if trees[curr_row][j] > tree_row[j]:
-          return recurse_up(curr_row - 1)
-        return False
-      pass
-
-  done_with_left_right = False
-  for j in range(len(tree_row) - 1, len(tree_row) / 2, -1):
-    if j == len(tree_row - 1):
-      # Don't count first and last tree
-      visible_count += 1
-      continue
-
-    if not done_with_left_right and next_tree <= tree_row[j]:
-      visible_count += 1
-      next_tree = tree_row[j]
-    else:
-      # All others to the halway are not visible left to right
-      done_with_left_right = True
-      break
-
-# middle_trees_row_index = len(trees) / 2
-next_row = len(trees) / 2
-
-for i in range(len(trees) / 2):
-  if i == 0:
-    # Don't count the first and last row
-    visible_count += len(trees)
-    continue
-
-  checkrow()
-
-for i in range(len(trees) - 1, len(trees) / 2, -1):
-  if i == len(trees - 1):
-    # Don't count the first and last row
-    visible_count += len(trees)
-    continue
-
-  checkrow()
+print(visible_count)
