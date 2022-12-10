31e013da145e1f6bac090b6a4e24cccdb37efac3
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-10 12:20:02 -0600

    part 1 complete. will do part 2 later

--- ten/main.py
+++ ten/main.py
@@ -2,4 +2,22 @@ def doit(input_file):
   with open(input_file, "r+") as f:
     lines = f.readlines()
 
+  total = 0
+  cycle = 1
+  x     = 1
+  for instr in lines:
+    cycle += 1
+
+    if instr.startswith('addx'):
+      if cycle % 40 == 20:
+        total += cycle * x
+
+      cycle += 1
+      x     += int(instr[5:])
+
+    if cycle % 40 == 20:
+      total += cycle * x
+
+  return (x, total)
+
 print(doit("input.txt"))
