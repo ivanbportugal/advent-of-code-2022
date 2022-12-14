e59d4e0fdfe9bc86adb88a9a41ebef814d50778d
Ivan Portugal <ivanbportugal@gmail.com>
2022-12-13 20:48:32 -0600

    onto 11

--- ten/main.py
+++ ten/main.py
@@ -5,19 +5,38 @@ def doit(input_file):
   total = 0
   cycle = 1
   x     = 1
+
+  crt = []
+  row = ''
   for instr in lines:
+    if x <= cycle % 40 <= x + 2:
+      row += '#'
+    else:
+      row += ' '
     cycle += 1
 
     if instr.startswith('addx'):
       if cycle % 40 == 20:
         total += cycle * x
+      elif cycle % 40 == 1:
+        crt.append(row)
+        row = ''
+
+      if x <= cycle % 40 <= x + 2:
+        row += '#'
+      else:
+        row += ' '
+
 
       cycle += 1
       x     += int(instr[5:])
 
     if cycle % 40 == 20:
       total += cycle * x
+    elif cycle % 40 == 1:
+      crt.append(row)
+      row = ''
 
-  return (x, total)
+  return '\n'.join(crt)
 
 print(doit("input.txt"))
